2b5520945493449b63f7b1c2c5df0cc5878e06fa
EugeneShab <dev.eugene.shab@gmail.com>
2018-03-15 16:03:39 +0200

    TJ-10: Refactoring after merged the branch TJ-10.
    Previously:
    - 'Foreach' processing about 4 sec.
    - Script strange affected the product stock.

--- app/code/IntechSoft/CustomImport/Plugin/SaveProductEntityPlugin.php
+++ app/code/IntechSoft/CustomImport/Plugin/SaveProductEntityPlugin.php
@@ -1,83 +1,88 @@
 <?php
 namespace IntechSoft\CustomImport\Plugin;
 
-class SaveProductEntityPlugin{
+use Magento\CatalogImportExport\Model\Import\Product as ImportProduct;
+use Magento\Framework\Registry;
+use Magento\Framework\App\ResourceConnection;
 
-    protected $_stockRegistry;
-    protected $_om;
-    protected $_resource;
-    protected $_connection;
+/**
+ * Class SaveProductEntityPlugin
+ *
+ * @package IntechSoft\CustomImport\Plugin
+ */
+class SaveProductEntityPlugin
+{
+    /**
+     * @var Registry
+     */
+    private $registry;
+    /**
+     * @var ResourceConnection
+     */
+    private $resource;
 
+    /**
+     * @var \Zend\Log\Logger
+     */
+    private $logger;
+
+    /**
+     * SaveProductEntityPlugin constructor.
+     *
+     * @param Registry           $registry
+     * @param ResourceConnection $resource
+     */
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $om,
-        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
-        \Magento\Framework\App\ResourceConnection $resource
+        Registry $registry,
+        ResourceConnection $resource
     ) {
-        $this->_resource = $resource;
-        $this->_stockRegistry = $stockRegistry;
-        $this->_om = $om;
+        $this->resource = $resource;
+        $this->registry = $registry;
+
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/change_stock_plugin.log');
+        $this->logger = new \Zend\Log\Logger();
+        $this->logger->addWriter($writer);
     }
 
-    public function afterSaveProductEntity(\Magento\CatalogImportExport\Model\Import\Product $subject, $result){
-        $registryImportFlag = $this->_om->get('\Magento\Framework\Registry');
-        if ($result instanceof \Magento\CatalogImportExport\Model\Import\Product) {
-            $registryImportFlag->register('importSuccessFlag', 1, true);
+    /**
+     * @param ImportProduct $subject
+     * @param               $result
+     * @return mixed
+     */
+    public function afterSaveProductEntity(ImportProduct $subject, $result){
+        if ($result instanceof ImportProduct) {
+            $this->registry->register('importSuccessFlag', 1, true);
         }
         return $result;
 
     }
 
-    public function beforeSaveProductEntity(\Magento\CatalogImportExport\Model\Import\Product $subject, $params){
-        $registryFirstSaveImportFlag = $this->_om->get('\Magento\Framework\Registry');
-        if ($registryFirstSaveImportFlag->registry('isFirstSaveImportFlag')) {
+    /**
+     * @param ImportProduct $subject
+     * @param               $params
+     * @return array
+     */
+    public function beforeSaveProductEntity(ImportProduct $subject, $params)
+    {
+        if ($this->registry->registry('isFirstSaveImportFlag')) {
             return [$params];
         }
-        $registryFirstSaveImportFlag->register('isFirstSaveImportFlag', 1, false);
-        $this->_connection = $this->_resource->getConnection();
-        $table=$this->_resource->getTableName('cataloginventory_stock_item');
-        $productCollection = $this->_om->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
-        foreach ($productCollection as $product) {
 
+        $this->registry->register('isFirstSaveImportFlag', 1, false);
 
-            $id = $product->getEntityId();
-            /*$this->_connection->update(
-                $table,
-                ["qty"  => new \Zend_Db_Expr('0')],
-                ["item_id = $id"]
+//        $this->_logger->info('Start stock update');
+//        $startTime = microtime(true);
 
-            );*/
-            $query = "update " . $table . " set qty = 0 where product_id = " . $id;
-            $this->_connection->query($query);
-            //->where(
-            //   'item_id = ?', $product->getEntityId());
+        $connection = $this->resource->getConnection();
 
-           /* $product->setStockData(array(
-                    'is_in_stock' => 1, //Stock Availability
-                    'qty' => 0 //qty
-                )
-            );
-            $product->save();*/
+        $select = $connection->select()->from($this->resource->getTableName('catalog_product_entity'), 'entity_id')
+            ->where('type_id IN(?)', ['configurable']);
 
+        $values = ['qty' => 0, 'is_in_stock' => 0, 'stock_status_changed_auto' => 1];
+        $where = sprintf('product_id IN (%1$s)', $select->assemble());
+        $connection->update($this->resource->getTableName('cataloginventory_stock_item'), $values, $where);
 
-
-            //$product->setQuantityAndStockStatus(['qty' => 0, 'is_in_stock' => 0]);
-           /* $productStockData = $this->_stockRegistry->getStockItem($product->getId());
-            $productStockData->setData('is_in_stock', 0);
-            $productStockData->setData('qty', 0);
-            $productStockData->setData('manage_stock', 1);
-            $productStockData->save();*/
-
-          //  $stockItem=$this->_stockRegistry->getStockItem($item['product_id']);
-            /*$stockItem=$this->_stockRegistry->getStockItem($item['product_id']); // load stock of that product
-            $stockItem->setData('is_in_stock',$stockData['is_in_stock']); //set updated data as your requirementx
-            $stockItem->setData('qty',$stockData['qty']); //set updated quantity
-            $stockItem->setData('manage_stock',$stockData['manage_stock']);
-            $stockItem->setData('use_config_notify_stock_qty',1);
-            $stockItem->save(); //save stock of item
-            $product->save(); //  also save product*/
-        }
-
+//        $resultTime = microtime(true) - $startTime;
+//        $this->_logger->info('Result time: ' . number_format($resultTime, 3) . ' sec');
     }
-
-
 }
